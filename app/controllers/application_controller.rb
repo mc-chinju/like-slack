d702b6b074409a0a0d7c4e834557095ad33f5f75
mc-chinju <cat.algorithm@gmail.com>
2017-07-06 01:27:06 +0900

    application_controller の修正, user および account の各種調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+  helper_method :current_enterprise, :current_account
+
+  def current_account
+    Account.eager_load(:enterprise).find_by(id: session[:account_id])
+  end
+
+  def current_enterprise
+    current_account.enterprise
+  end
 end
--- db/migrate/20170704011653_create_accounts.rb
+++ db/migrate/20170704011653_create_accounts.rb
@@ -1,11 +1,12 @@
 class CreateAccounts < ActiveRecord::Migration[5.1]
   def change
     create_table :accounts do |t|
-      t.integer, :user_id
-      t.integer, :enterprise_id
+      t.integer :user_id
+      t.integer :enterprise_id
       t.integer :role
 
       t.timestamps
+      t.index [:user_id, :enterprise_id], unique: true, name: "index_accounts_on_user_and_enterprise"
     end
   end
 end
