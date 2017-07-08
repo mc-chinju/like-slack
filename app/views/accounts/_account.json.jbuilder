b9f7a23c3376e0285fd4ee85d3baaf436510d557
mc-chinju <cat.algorithm@gmail.com>
2017-07-09 00:52:37 +0900

    conflict を解消

--- app/views/accounts/_account.json.jbuilder
+++ app/views/accounts/_account.json.jbuilder
@@ -1,5 +1,1 @@
 json.extract! account, :id, :user_id, :enterprise_id, :role, :created_at, :updated_at
-<<<<<<< HEAD
-=======
-
->>>>>>> 1bbe0c5... userテーブルの作成
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  resources :enterprises
-  devise_for :users
-  resources :accounts
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-=======
   root to: "main#main"
+  resources :enterprises
   devise_for :users
   # devise_for :users, only: [:sign_in, :sign_out, :session], controllers: {
   #   registrations: :'users/registrations',
   #   sessions:      :'users/sessions'
   # }
->>>>>>> 0e3fa73353bca40df1a4fd50bf466a0ddc9e1542
 end
