caf83daca0927f0957874aeddd94718deb5b40b3
Soukichi <ovue61@yahoo.co.jp>
2017-07-15 11:02:37 +0900

    fixed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,26 @@
+#TODO: リリース前には削除
+
 User.delete_all
 Account.delete_all
 Enterprise.delete_all
 Channel.delete_all
 
-owner = User.new(:login => 'owhoge',:email => 'owner@hoge.com', :password => 'hugahuga')
-owner.save!
+owner = User.create!(login:'owhoge',email:'owner@hoge.com', password:'hugahuga')
 
-user = User.new(:login => 'hoge',:email => 'hogehoge@hoge.com', :password => 'hugahuga')
-user.save!
+user = User.create!(login:'hoge',email:'hogehoge@hoge.com',password:'hugahuga')
 
-enterprise = Enterprise.new(
-  :name => 'nigekiri', :account_name => '逃げ切り'
+enterprise = Enterprise.create!(name:'nigekiri',account_name:'逃げ切り')
+
+account1 = Account.create!(user_id:owner.id,enterprise_id:enterprise.id)
+
+Account.create!(user_id:user.id,enterprise_id:enterprise.id)
+
+channel1 = Channel.create!(
+  enterprise_id:enterprise.id,
+  owner_id:owner.id,
+  name:'test_channel',
+  user_id:user.id
 )
-enterprise.save!
-
-account1 = Account.new(
-  :user_id => owner.id,:enterprise_id => enterprise.id
-)
-account1.save!
-
-Account.create!(
-  :user_id => user.id,:enterprise_id => enterprise.id
-)
-
-channel1 = Channel.new(
-  :enterprise_id => enterprise.id,
-  :owner_id => owner.id,
-  :name => 'test_channel',
-  :user_id => user.id
-)
-channel1.save!
 
 Message.create!(
   :body => 'Hello!',
